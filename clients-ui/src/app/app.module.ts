87453e624017e1e00fb76cf9f9183602539fc582
Jeoffrey Fischer <jeoffrey.fischer@gmail.com>
2023-08-14 07:37:17 +1000

    ui-imports

--- clients-ui/src/app/app.module.ts
+++ clients-ui/src/app/app.module.ts
@@ -5,15 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ClientsListComponent } from './components/clients-list/clients-list.component';
+import { ClientFormComponent } from './components/client-form/client-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsListComponent,
+    ClientFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
